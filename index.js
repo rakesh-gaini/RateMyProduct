e7554ebc65fb4b8e4d67fc98d875c79ab5a9786c
RakeshGaini <gainirakesh123@gmail.com>
2024-03-09 17:14:54 -0500

    separation of services and routes logic

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+require('./services/passport')
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello, I'm Rakesh!");
-})
+require('./routes/authRoutes')(app);
 
-app.listen(8080, ()=> {
-    console.log("Listening on 8080");
+// app.get('/', (req, res) => {
+//     res.send("Hello, I'm Rakesh!");
+// })
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, ()=> {
+    console.log("Listening on "+PORT);
 })
